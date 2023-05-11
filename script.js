be468a3dab6a428c859a25eb2e0ca13e4048a23c
﻿Abiodun Busari <babiodun@afexnigeria.com>
2023-05-11 21:10:11 +0100

    Made the hamburger menu functional

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-hamburger.classList.remove("active");
-navMenu.classList.remove("active");
-}
+const menu_bar = document.querySelector(".hamburger"); const mobile_menu = document.querySelector(".mobile-nav"); menu_bar.addEventListener("click", function () { menu_bar.classList.toggle("is-active"); mobile_menu.classList.toggle("is-active"); });
